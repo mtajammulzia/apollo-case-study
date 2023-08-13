5eaeb1fbe51ddb2e8874be502c5114d1d295a598
tajammul <mtajammulzia@gmail.com>
2023-08-14 03:20:30 +0500

    Internet connection check

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import { AuthProvider } from "./contexts/Auth";
 import Router from "./router";
 
 function App() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    function onlineHandler() {
+      setIsOnline(true);
+    }
+
+    function offlineHandler() {
+      setIsOnline(false);
+    }
+
+    window.addEventListener("online", onlineHandler);
+    window.addEventListener("offline", offlineHandler);
+
+    return () => {
+      window.removeEventListener("online", onlineHandler);
+      window.removeEventListener("offline", offlineHandler);
+    };
+  }, []);
+
+  if (!isOnline) {
+    return <h1>Oops! You seem to be disconnected from the Internet</h1>;
+  }
+
   return (
     <AuthProvider>
       <Router />
